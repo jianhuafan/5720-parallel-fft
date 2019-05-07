94c58fe95f52b71ac5194e4754fa2f691b71eb43
jf773 <jf773@cornell.edu>
2019-05-07 00:10:18 -0400

    add openmp

--- openmp_fft.c
+++ openmp_fft.c
@@ -167,7 +167,6 @@ void openmp_fft(Complex *in, Complex *out, int n) {
                 if (!(i & step)) {
                     Complex u = out[i];
                     Complex t = comp_mul(W[(i * a) % (step * a)], out[i + n]);
-                    printf("%d\n", i);
                     Complex *even_ptr = out + i;
                     Complex *odd_ptr = out + i + step;
                     even_ptr->a = u.a + t.a;
@@ -179,6 +178,7 @@ void openmp_fft(Complex *in, Complex *out, int n) {
             step <<= 1;
             a >>= 1;
         }
+        printf("%d\n", j);
     }
 }
 
