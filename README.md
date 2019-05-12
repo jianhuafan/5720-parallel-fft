12e89a6a746378ee68c93665746e2541b2bf19c1
jf773 <jf773@cornell.edu>
2019-05-12 11:17:58 -0400

    change to 2d fft

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 compile command
 ```
-$ /usr/local/cuda-10.0/bin/nvcc -arch=compute_35 cuda_fft.cu -lcublas
+$ /usr/local/cuda-10.0/bin/nvcc -arch=compute_35 cuda_fft.cu -lcublas -lcufft
 ```
--- cuda_fft.cu
+++ cuda_fft.cu
@@ -1,4 +1,4 @@
-// compile command: /usr/local/cuda-10.0/bin/nvcc -arch=compute_35 cuda_fft.cu -lcublas
+// compile command: /usr/local/cuda-10.0/bin/nvcc -arch=compute_35 cuda_fft.cu -lcublas -lcufft
 
 #include <math.h>
 #include <stdio.h>
@@ -14,39 +14,41 @@
 
 typedef float2 Complex;
 
-#define DIM 65536
+#define N 4
 
 int main(int argc, char **argv) {
     float elapsedTime = 0;
     cufftHandle plan;
-    cufftComplex *host_data = (cufftComplex*)malloc(DIM * sizeof(cufftComplex));
+    cufftComplex *host_data = (cufftComplex*)malloc(N*N * sizeof(cufftComplex));
     cufftComplex *dev_data;
     cudaEvent_t start, stop;
 
     // feed input
     srand(time(NULL));
-    for (int i = 0; i < DIM; i++) {
-        host_data[i].x = rand() / (float) RAND_MAX;
-        host_data[i].y = 0.0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; k++) {
+            host_data[i * N + j].x = rand() / (float) RAND_MAX;
+            host_data[i * N + j].y = 0.0;
+        }
     }
 
     // allocate gpu memory
-    cudaMalloc((void**)&dev_data, sizeof(cufftComplex) * DIM);
+    cudaMalloc((void**)&dev_data, sizeof(cufftComplex) * N*N);
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
 
     // copy input to device
-    cudaMemcpy(dev_data, host_data, DIM * sizeof(cufftComplex), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_data, host_data, N*N * sizeof(cufftComplex), cudaMemcpyHostToDevice);
     
     // create cufft plan
-    cufftPlan1d(&plan, DIM, CUFFT_C2C, 1);
+    cufftPlan2D(&plan, N, N, CUFFT_C2C, 1);
 
     // perform computation
     cufftExecC2C(plan, dev_data, dev_data, CUFFT_FORWARD);
 
     // copy back results
-    cudaMemcpy(host_data, dev_data, sizeof(cufftComplex) * DIM, cudaMemcpyDeviceToHost);
+    cudaMemcpy(host_data, dev_data, sizeof(cufftComplex) * N*N, cudaMemcpyDeviceToHost);
 
     // get calculation time
     cudaEventRecord(stop, 0);
@@ -54,9 +56,12 @@ int main(int argc, char **argv) {
     cudaEventElapsedTime(&elapsedTime, start, stop);
 
     // show results
-    for (int i = 0; i < 16; i++) {
-        printf("DATA: %3.1f %3.1f \n", host_data[i].x, host_data[i].y);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("DATA: %3.1f %3.1f \n", host_data[i].x, host_data[i].y);
+        }
     }
+    printf("CUFFT calculation completed: %5.3f ms\n", elapsedTime);
 
     // free memory
     cufftDestroy(plan);
