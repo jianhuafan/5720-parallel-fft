4b8bcb278b4064bc7fec432bb1d10fd3d75e58fd
jf773 <jf773@cornell.edu>
2019-05-12 13:05:56 -0400

    add filter

--- cuda_fft.cu
+++ cuda_fft.cu
@@ -16,39 +16,56 @@ typedef float2 Complex;
 
 #define N 4
 
+__global__ void ComplexMul(cufftComplex *a, cufftComplex *b) {
+    int i = threadIdx.x;
+    a[i].x = a[i].x * b[i].x - a[i].y*b[i].y;
+    a[i].y = a[i].x * b[i].y + a[i].y*b[i].x;
+}
+
 int main(int argc, char **argv) {
     float elapsedTime = 0;
     cufftHandle plan;
-    cufftComplex *host_data = (cufftComplex*)malloc(N*N * sizeof(cufftComplex));
-    cufftComplex *dev_data;
+    int mem_size = N*N * sizeof(cufftComplex);
+    cufftComplex *signal = (cufftComplex*)malloc(mem_size);
+    cufftComplex *filter_kernel = (cufftComplex*)malloc(mem_size);
+    cufftComplex *dev_signal;
+    cufftComplex *dev_filter_kernel;
     cudaEvent_t start, stop;
 
     // feed input
     srand(time(NULL));
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            host_data[i * N + j].x = rand() / (float) RAND_MAX;
-            host_data[i * N + j].y = 0.0;
+            signal[i * N + j].x = rand() / (float) RAND_MAX;
+            signal[i * N + j].y = 0.0;
+            filter_kernel[i * N + j].x = rand() / (float) RAND_MAX;
+            filter_kernel[i * N + j].y = 0.0;
         }
     }
 
     // allocate gpu memory
-    cudaMalloc((void**)&dev_data, sizeof(cufftComplex) * N*N);
+    cudaMalloc((void**)&dev_signal, mem_size);
+    cudaMalloc((void**)&dev_filter_kernel, mem_size);
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start, 0);
 
     // copy input to device
-    cudaMemcpy(dev_data, host_data, N*N * sizeof(cufftComplex), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_signal, signal, mem_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_filter_kernel, filter_kernel, mem_size, cudaMemcpyHostToDevice);
     
     // create cufft plan
     cufftPlan2d(&plan, N, N, CUFFT_C2C);
 
-    // perform computation
-    cufftExecC2C(plan, dev_data, dev_data, CUFFT_FORWARD);
+    // perform 2dfft
+    cufftExecC2C(plan, dev_signal, dev_signal, CUFFT_FORWARD);
+    cufftExecC2C(plan, dev_filter_kernel, dev_filter_kernel, CUFFT_FORWARD);
 
-    // copy back results
-    cudaMemcpy(host_data, dev_data, sizeof(cufftComplex) * N*N, cudaMemcpyDeviceToHost);
+    // perform multiplication
+    ComplexMul <<<32, 256>>>(dev_signal, dev_filter_kernel);
+
+    // perform inverse 2dfft
+    cufftExecC2C(plan, dev_signal, dev_signal, CUFFT_INVERSE);
 
     // get calculation time
     cudaEventRecord(stop, 0);
@@ -58,15 +75,17 @@ int main(int argc, char **argv) {
     // show results
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            printf("DATA: %3.1f %3.1f \n", host_data[i].x, host_data[i].y);
+            printf("DATA: %3.1f %3.1f \n", signal[i].x, signal[i].y);
         }
     }
     printf("CUFFT calculation completed: %5.3f ms\n", elapsedTime);
 
     // free memory
     cufftDestroy(plan);
-    cudaFree(dev_data);
-    free(host_data);
+    cudaFree(dev_signal);
+    cudaFree(dev_filter_kernel);
+    free(signal);
+    free(filter_kernel);
 
     return 0;
 }
