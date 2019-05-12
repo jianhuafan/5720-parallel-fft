222de808690b6f782d3e84fff402b8efe283842d
jf773 <jf773@cornell.edu>
2019-05-12 13:46:24 -0400

    add image write

--- cuda_fft.cu
+++ cuda_fft.cu
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <stdint.h>
 
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
@@ -12,20 +13,33 @@
 #include <cufft.h>
 #include <cufftXt.h>
 
-typedef float2 Complex;
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
-#define N 4
+#define FILTER_KERNEL_SIZE 11
 
-__global__ void ComplexMul(cufftComplex *a, cufftComplex *b) {
-    int i = threadIdx.x;
-    a[i].x = a[i].x * b[i].x - a[i].y*b[i].y;
-    a[i].y = a[i].x * b[i].y + a[i].y*b[i].x;
+__global__ void ComplexMul(cufftComplex *a, cufftComplex *b, int size) {
+    const int numThreads = blockDim.x * gridDim.x;
+    const int threadID = blockIdx.x * blockDim.x + threadIdx.x;
+    for (int i = threadID; i < size; i += numThreads) {
+        cufftComplex c;
+        c.x = a[i].x * b[i].x - a[i].y*b[i].y;
+        c.y = a[i].x * b[i].y + a[i].y*b[i].x;
+        a[i] = c;
+    }
 }
 
 int main(int argc, char **argv) {
+
+    // load image
+    int width, height, bpp;
+    uint8_t* rgb_image = stbi_load("image/dog.jpg", &width, &height, &bpp, STBI_grey);
+
     float elapsedTime = 0;
     cufftHandle plan;
-    int mem_size = N*N * sizeof(cufftComplex);
+    int mem_size = width * height * sizeof(cufftComplex);
     cufftComplex *signal = (cufftComplex*)malloc(mem_size);
     cufftComplex *filter_kernel = (cufftComplex*)malloc(mem_size);
     cufftComplex *dev_signal;
@@ -34,13 +48,17 @@ int main(int argc, char **argv) {
 
     // feed input
     srand(time(NULL));
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            signal[i * N + j].x = rand() / (float) RAND_MAX;
-            signal[i * N + j].y = 0.0;
-            filter_kernel[i * N + j].x = rand() / (float) RAND_MAX;
-            filter_kernel[i * N + j].y = 0.0;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            uint8_t* pixel = rgb_image + (i * width + j) * bpp;
+            signal[i * width + j].x = (float)pixel[0];
+            signal[i * width + j].y = 0.0;
         }
+    }
+    // feed kernel
+    for (int i = 0; i < FILTER_KERNEL_SIZE; i++) {
+        filter_kernel[i].x = rand() / (float) RAND_MAX;
+        filter_kernel[i].y = 0.0;
     }
 
     // allocate gpu memory
@@ -62,10 +80,13 @@ int main(int argc, char **argv) {
     cufftExecC2C(plan, dev_filter_kernel, dev_filter_kernel, CUFFT_FORWARD);
 
     // perform multiplication
-    ComplexMul <<<32, 256>>>(dev_signal, dev_filter_kernel);
+    ComplexMul <<<32, 256>>>(dev_signal, dev_filter_kernel, width * height);
 
     // perform inverse 2dfft
     cufftExecC2C(plan, dev_signal, dev_signal, CUFFT_INVERSE);
+
+    // copy back results
+    cudaMemcpy(signal, dev_signal, mem_size, cudaMemcpyDeviceToHost);
 
     // get calculation time
     cudaEventRecord(stop, 0);
@@ -73,12 +94,22 @@ int main(int argc, char **argv) {
     cudaEventElapsedTime(&elapsedTime, start, stop);
 
     // show results
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("DATA: %3.1f %3.1f \n", signal[i].x, signal[i].y);
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("DATA: %3.1f %3.1f \n", signal[i * 4 + j].x, signal[i * 4 + j].y);
         }
     }
     printf("CUFFT calculation completed: %5.3f ms\n", elapsedTime);
+
+    // write filtered image
+    uint8_t* output_rgb_image;
+    output_rgb_image = malloc(width*height);
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            output_rgb_image[i * width + j] = (uint8_t)signal[i * width + j];
+        }
+    }
+    stbi_write_png("image/filtered_dog.png", width, height, 1, output_rgb_image, width);
 
     // free memory
     cufftDestroy(plan);
@@ -86,6 +117,7 @@ int main(int argc, char **argv) {
     cudaFree(dev_filter_kernel);
     free(signal);
     free(filter_kernel);
+    stbi_image_free(rgb_image);
 
     return 0;
 }
