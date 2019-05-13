816b76991b598b1973cd120b8f79f33e5d2d0f5b
jf773 <jf773@cornell.edu>
2019-05-13 16:06:21 -0400

    fix

--- test.c
+++ test.c
@@ -8,47 +8,53 @@
 #include "self_openmp_fft_lib.h"
 #include <fftw3.h>
 
-#define N 4
-
 int main() {
+
+    // load image
+    int width, height, bpp;
+    uint8_t* grey_image = stbi_load("input/sheep.png", &width, &height, &bpp, STBI_grey);
+    printf("input image, width: %d, height: %d\n", width, height);
 
     // feed input
     fftw_complex *fftw_input;
     Complex *self_input;
-    fftw_input = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)* N*N);
-    self_input = (Complex*) malloc(sizeof(Complex)* N*N);
-    srand(time(NULL));
-    for (int i = 0; i < N * N; i++) {
-        fftw_input[i][0] = rand();
-        fftw_input[i][1] = 0.0;
-        self_input[i].a = fftw_input[i][0];
-        self_input[i].b = 0.0;
+    fftw_input = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)* height*width);
+    self_input = (Complex*) malloc(sizeof(Complex)* height*width);
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            uint8_t* pixel = grey_image + (i * width + j);
+            self_input[i * width + j].a = (double)pixel[0];
+            self_input[i * width + j].b = 0.0;
+            fftw_input[i * width + j][0] = (double)pixel[0];
+            fftw_input[i * width + j][1] = 0.0;
+        }
     }
 
     // perform fftw 2d fft
     fftw_complex * fftw_output;
-    fftw_output = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)* N*N);
+    fftw_output = (fftw_complex*) fftw_malloc(sizeof(fftw_complex)* height*width);
     fftw_plan my_plan;
-    my_plan = fftw_plan_dft_2d(N, N, fftw_input, fftw_output, FFTW_BACKWARD, FFTW_ESTIMATE);
+    my_plan = fftw_plan_dft_2d(height, width, fftw_input, fftw_output, FFTW_BACKWARD, FFTW_ESTIMATE);
     fftw_execute(my_plan);
 
     // perform self 2d fft
     Complex *self_output;
-    self_output = (Complex*) malloc(sizeof(Complex)* N*N);
-    openmp_2d_fft(self_input, self_output, N, N, FFT_BACKWARD);
+    self_output = (Complex*) malloc(sizeof(Complex)* height*width);
+    openmp_2d_fft(self_input, self_output, height, width, FFT_BACKWARD);
 
     // compare result
     printf("=======fftw result=======\n");
-    for (int i = 0; i < N*N; i++) {
+    for (int i = 0; i < 8; i++) {
         printf("DATA: %3.1f %3.1f\n", fftw_output[i][0], fftw_output[i][1]);
     }
     printf("======self result=======\n");
-    for (int i = 0; i < N*N; i++) {
+    for (int i = 0; i < 8; i++) {
         printf("DATA: %3.1f %3.1f\n", self_output[i].a, self_output[i].b);
     }
 
     int wrong_result = 0;
-    for (int i = 0; i < N*N; i++) {
+    for (int i = 0; i < 8; i++) {
         if (abs(fftw_output[i][0] - self_output[i].a) > 1e-5) {
             wrong_result = 1;
             break;
