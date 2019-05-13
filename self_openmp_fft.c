731ad06f084843545d71eccffcdc793509cdce99
jf773 <jf773@cornell.edu>
2019-05-13 13:39:45 -0400

    add self implemented image filter

---
+++ self_openmp_fft.c
@@ -0,0 +1,208 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdint.h>
+
+#include "self_openmp_fft_lib.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#define FILTER_KERNEL_SIZE 9
+#define BILLION 1000000000L
+
+// Pad data
+int PadData(const Complex *signal, Complex **padded_signal, int signal_size,
+            const Complex *filter_kernel, Complex **padded_filter_kernel,
+            int filter_kernel_size) {
+  int minRadius = filter_kernel_size / 2;
+  int maxRadius = filter_kernel_size - minRadius;
+  int new_size = signal_size + maxRadius;
+
+  // Pad signal
+  Complex *new_data =(Complex *)(malloc(sizeof(Complex) * new_size));
+  memcpy(new_data + 0, signal, signal_size * sizeof(Complex));
+  memset(new_data + signal_size, 0, (new_size - signal_size) * sizeof(Complex));
+  *padded_signal = new_data;
+
+  // Pad filter
+  new_data = (Complex *)(malloc(sizeof(Complex) * new_size));
+  memcpy(new_data + 0, filter_kernel + minRadius, maxRadius * sizeof(Complex));
+  memset(new_data + maxRadius, 0,
+         (new_size - filter_kernel_size) * sizeof(Complex));
+  memcpy(new_data + new_size - minRadius, filter_kernel,
+         minRadius * sizeof(Complex));
+  *padded_filter_kernel = new_data;
+
+  return new_size;
+}
+
+void feed_gaussian_kernel(Complex *filter_kernel, int filter_kernel_size) {
+    for (int i = 0; i < FILTER_KERNEL_SIZE; i++) {
+        filter_kernel[i].a = 0.0;
+        filter_kernel[i].b = 0.0;
+    }
+    filter_kernel[0].a = 1.0f / 16;
+    filter_kernel[1].a = 2.0f / 16;
+    filter_kernel[2].a = 1.0f / 16;
+    filter_kernel[3].a = 2.0f / 16;
+    filter_kernel[4].a = 4.0f / 16;
+    filter_kernel[5].a = 2.0f / 16;
+    filter_kernel[6].a = 1.0f / 16;
+    filter_kernel[7].a = 2.0f / 16;
+    filter_kernel[8].a = 1.0f / 16;
+}
+
+void feed_identity_kernel(Complex *filter_kernel, int filter_kernel_size) {
+    for (int i = 0; i < FILTER_KERNEL_SIZE; i++) {
+        filter_kernel[i].a = 0.0;
+        filter_kernel[i].b = 0.0;
+        if (i == FILTER_KERNEL_SIZE / 2) {
+            filter_kernel[i].a = 1.0;
+        }
+    }
+}
+
+void feed_edge_detection_kernel(Complex *filter_kernel, int filter_kernel_size) {
+    for (int i = 0; i < FILTER_KERNEL_SIZE; i++) {
+        filter_kernel[i].a = 0.0;
+        filter_kernel[i].b = 0.0;
+    }
+    filter_kernel[0].a = -1.0f;
+    filter_kernel[1].a = -1.0f;
+    filter_kernel[2].a = -1.0f;
+    filter_kernel[3].a = -1.0f;
+    filter_kernel[4].a = 8.0f;
+    filter_kernel[5].a = -1.0f;
+    filter_kernel[6].a = -1.0f;
+    filter_kernel[7].a = -1.0f;
+    filter_kernel[8].a = -1.0f;
+}
+
+void feed_box_blur_kernel(Complex *filter_kernel, int filter_kernel_size) {
+    for (int i = 0; i < FILTER_KERNEL_SIZE; i++) {
+        filter_kernel[i].a = 1.0 / filter_kernel_size;
+        filter_kernel[i].b = 0.0;
+    }
+}
+
+void feed_sharpen_kernel(Complex *filter_kernel, int filter_kernel_size) {
+    for (int i = 0; i < FILTER_KERNEL_SIZE; i++) {
+        filter_kernel[i].a = 0.0;
+        filter_kernel[i].b = 0.0;
+    }
+    filter_kernel[1].a = -1.0f;
+    filter_kernel[3].a = -1.0f;
+    filter_kernel[4].a = 5.0f;
+    filter_kernel[5].a = -1.0f;
+    filter_kernel[7].a = -1.0f;
+}
+
+void ComplexMul(Complex *a, Complex *b, int size) {
+    for (int i = 0; i < size; i++) {
+        Complex c;
+        c.a = a[i].a * b[i].a - a[i].b*b[i].b;
+        c.b = a[i].a * b[i].b + a[i].b*b[i].a;
+        c.a /= size;
+        c.b /= size;
+        a[i].a = c.a;
+        a[i].b = c.b;
+    }
+}
+
+int main(int argc, char **argv) {
+
+    // load image
+    int width, height, bpp;
+    uint8_t* grey_image = stbi_load("image/sheep.png", &width, &height, &bpp, STBI_grey);
+
+    Complex *signal;
+    Complex *filter_kernel;
+    signal = (Complex*) malloc(sizeof(Complex)* height * width);
+    filter_kernel = (Complex*) malloc(sizeof(Complex)* FILTER_KERNEL_SIZE);
+
+    // feed input
+    srand(time(NULL));
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            uint8_t* pixel = grey_image + (i * width + j);
+            signal[i * width + j].a = (double)pixel[0];
+            signal[i * width + j].b = 0.0;
+        }
+    }
+
+    // feed kernel
+    feed_sharpen_kernel(filter_kernel, FILTER_KERNEL_SIZE);
+
+    // pad image and filter kernel
+    Complex *padded_signal;
+    Complex *padded_filter_kernel;
+    int new_size = PadData(signal, &padded_signal, width * height, filter_kernel,
+              &padded_filter_kernel, FILTER_KERNEL_SIZE);
+
+    //have output buffer
+    Complex *out_signal;
+    Complex *out_filter_kernel;
+    out_signal = (Complex*) malloc(sizeof(Complex)* new_size);
+    out_filter_kernel = (Complex*) malloc(sizeof(Complex)* new_size);
+
+    // convolution starts
+    struct timespec start, end;
+    long long unsigned int diff;
+    clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
+
+    // perform 2d fft
+    openmp_2d_fft(padded_signal, out_signal, height, width, FFT_FORWARD);
+    openmp_2d_fft(padded_filter_kernel, out_filter_kernel, height, width, FFT_FORWARD);
+
+    // perform multiplication
+    ComplexMul(out_signal, out_filter_kernel, new_size);
+
+    // perform inverse fft
+    openmp_2d_fft(out_signal, padded_signal, height, width, FFT_BACKWARD);
+
+    // convolution ends
+    clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
+    diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+	printf("elapsed time = %llu us\n", (long long unsigned int) (diff / 1000));
+
+    // print result
+    printf("results\n");
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("DATA: %3.1f %3.1f\n", padded_signal[i * 4 + j].a, padded_signal[i * 4 + j].b);
+        }
+    }
+
+    // write filtered image
+    uint8_t* output_grey_image;
+    output_grey_image = (uint8_t*)malloc(width*height);
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            output_grey_image[i * width + j] = (uint8_t)padded_signal[i * width + j].a;
+            if (i < 4 && j < 4) {
+                printf("%hhu\n", output_grey_image[i * width + j]);
+            }
+        }
+    }
+    int result = stbi_write_png("image/fftw/filtered_sharpen_sheep.png", width, height, 1, output_grey_image, width);
+    if (!result) {
+        printf("error writing image!\n");
+    }
+
+    // free memory
+    free(signal);
+    free(padded_signal);
+    free(filter_kernel);
+    free(padded_filter_kernel);
+    free(out_signal);
+    free(out_filter_kernel);
+    stbi_image_free(grey_image);
+    stbi_image_free(output_grey_image);
+
+    return 0;
+}
---
+++ self_openmp_fft_lib.h
@@ -0,0 +1,232 @@
+/**  FFT using Cooley–Tukey FFT algorithm
+ *  flag: -n size of sequence, r resursive, i iterative
+ */ 
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <limits.h>
+#include <sys/times.h>
+#include <sys/types.h>
+#include <omp.h>
+
+#define BILLION 1000000000L
+#define FFT_FORWARD -1
+#define FFT_BACKWARD 1
+
+typedef struct Complex {
+    // a + bi
+    double a;
+    double b;
+} Complex;
+
+Complex comp_create(double a, double b) {
+    Complex ret;
+    ret.a = a;
+    ret.b = b;
+    return ret;
+}
+
+Complex comp_add(Complex c1, Complex c2) {
+    Complex ret = c1;
+    ret.a += c2.a;
+    ret.b += c2.b;
+    return ret;
+}
+
+Complex comp_sub(Complex c1, Complex c2) {
+    Complex ret = c1;
+    ret.a -= c2.a;
+    ret.b -= c2.b;
+    return ret;
+}
+
+Complex comp_mul(Complex c1, Complex c2) {
+    Complex ret;
+    ret.a = c1.a * c2.a - c1.b * c2.b;
+    ret.b = c1.b * c2.a + c1.a * c2.b;
+    return ret;
+}
+
+Complex comp_euler(double x) {
+    Complex ret;
+    ret.a = cos(x);
+    ret.b = sin(x);
+    return ret;
+}
+
+void comp_print(Complex comp) {
+    if (comp.b < 0) {
+        printf("%.6f - %.6f i\n", comp.a, -comp.b);
+    } else {
+        printf("%.6f + %.6f i\n", comp.a, comp.b);
+    }
+}
+
+void comp_mul_self(Complex *c1, Complex *c2) {
+    double c1a = c1->a;
+    c1->a = c1a * c2->a - c1->b * c2->b;
+    c1->b = c1->b * c2->a + c1a * c2->b;
+}
+
+void resursive_fft(const Complex *in, Complex *out, int step, int n) {
+    int i;
+    int half_n = n >> 1;
+    const double PI = acos(-1);
+    Complex ep = comp_euler(-PI / (double)half_n);
+    Complex ei;
+    Complex *ep_ptr = &ep;
+    Complex *ei_ptr = &ei;
+    if (!half_n) {
+        *out = *in;
+    } else {
+        resursive_fft(in, out, step << 1, half_n);
+        resursive_fft(in + step, out + half_n, step << 1, half_n);
+        ei_ptr->a = 1;
+        ei_ptr->b = 0;
+        for (i = 0; i < half_n; i++) {
+            Complex even = out[i];
+            Complex *even_ptr = out + i;
+            Complex *odd_ptr = even_ptr + half_n;
+            comp_mul_self(odd_ptr, ei_ptr);
+            even_ptr->a += odd_ptr->a;
+            even_ptr->b += odd_ptr->b;
+            odd_ptr->a = even.a - odd_ptr->a;
+            odd_ptr->b = even.b - odd_ptr->b;
+            comp_mul_self(ei_ptr, ep_ptr);
+        }
+    }
+
+}
+
+unsigned int bit_reverse(unsigned int num, unsigned int bits);
+void bit_reverse_array(Complex *in, Complex *out, int n);
+
+void iterative_fft(Complex *in, Complex *out, int n) {
+    int i, s, j, k;
+    const double PI = acos(-1);
+    bit_reverse_array(in, out, n);
+    Complex ei;
+    Complex *ei_ptr = &ei;
+    for (s = 1; s < log2(n) + 1; s++) {
+        int m = 1 << s;
+        int m2 = m >> 1;
+        Complex ep = comp_euler(-PI / (double)m2);
+        Complex *ep_ptr = &ep; 
+        ei_ptr->a = 1;
+        ei_ptr->b = 0;
+        for (j = 0; j < m2; j++) {
+            for (k = j; k < n; k += m) {
+                Complex t = comp_mul(ei, out[k + m2]);
+                Complex u = out[k];
+                Complex *even_ptr = out + k;
+                Complex *odd_ptr = out + k + m2;
+                even_ptr->a = u.a + t.a;
+                even_ptr->b = u.b + t.b;
+                odd_ptr->a = u.a - t.a;
+                odd_ptr->b = u.b - t.b;
+            }
+            comp_mul_self(ei_ptr, ep_ptr);
+        }
+    }
+}
+
+void init_w_table(Complex *W, int n, int sign) {
+    int i;
+    const double PI = acos(-1);
+    W[0] = comp_create(1, 0);
+    W[1] = comp_euler(sign * PI / (double)(n / 2));
+    #pragma omp parallel shared ( W ) private ( i )
+    {
+        #pragma omp for nowait
+        for (i = 2; i < n / 2; i++) {
+            W[i] = comp_create(1, 0);
+            int j;
+            for (j = 0; j < i; j++) {
+                comp_mul_self(&W[i], &W[1]);
+            }
+        }
+    }
+}
+
+void openmp_1d_fft(Complex *in, Complex *out, int n, int sign) {
+    unsigned long step = 1;
+    unsigned long i;
+    unsigned long a = n / 2;
+    unsigned long j;
+    const double PI = acos(-1);
+    bit_reverse_array(in, out, n);
+    Complex *W;
+    W = (Complex *) malloc(sizeof(Complex) * (size_t)a);
+    init_w_table(W, n, sign);
+    unsigned long size = log2(n);
+    for (j = 0; j < size; j++) {
+        #pragma omp parallel shared(j, in, out, W, step, a, n) private(i)
+        {
+            #pragma omp for
+            for (i = 0; i < n; i++) {
+                if (!(i & step)) {
+                    Complex u = out[i];
+                    Complex t = comp_mul(W[(i * a) % (step * a)], out[i + step]);
+                    Complex *even_ptr = out + i;
+                    Complex *odd_ptr = out + i + step;
+                    even_ptr->a = u.a + t.a;
+                    even_ptr->b = u.b + t.b;
+                    odd_ptr->a = u.a - t.a;
+                    odd_ptr->b = u.b - t.b;
+                }
+            }
+        }
+        step = step * 2;
+        a = a / 2;
+    }
+}
+
+void openmp_2d_fft(Complex *in, Complex *out, int m, int n, int sign) {
+    int i, j;
+    #pragma omp parallel
+    {
+        #pragma omp for private(i, j)
+        for (i = 0; i < m; i++) {
+            openmp_1d_fft(in + i * n, out + i * n, n, sign);
+        }
+        Complex* transposed_mat = (Complex *) malloc(sizeof(Complex) * m * n);
+        for (i = 0; i < m; i++) {
+            for (j = 0; j < n; j++) {
+                transposed_mat[j * n + i] = out[i * n + j];
+            }
+        }
+        for (i = 0; i < m; i++) {
+            openmp_1d_fft(transposed_mat + i * n, out + i * n, n, sign);
+        }
+    }
+}
+
+void bit_reverse_array(Complex *in, Complex *out, int n) {
+    unsigned int i;
+    unsigned int bits = log2(n);
+    #pragma omp parallel shared ( in, out, n, bits ) private ( i )
+    {
+        #pragma omp for nowait
+        for (i = 0; i < n; i++) {
+            int reversed_i = bit_reverse(i, bits);
+            Complex *out_ptr = out + reversed_i;
+            Complex *in_ptr = in + i;
+            out_ptr->a = in_ptr->a;
+            out_ptr->b = in_ptr->b;
+        }
+    }
+}
+
+unsigned int bit_reverse(unsigned int num, unsigned int bits) {
+    unsigned int reversed_num = 0;
+    unsigned int i;
+    for (i = 0; i < bits; i++) {
+        reversed_num <<= 1;
+        reversed_num |= (num & 1);
+        num >>= 1;
+    }
+    return reversed_num;
+}
