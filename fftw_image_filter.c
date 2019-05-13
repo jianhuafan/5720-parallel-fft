c59e7766c067d7e9c962a80b5dbb601cdba27eac
jf773 <jf773@cornell.edu>
2019-05-13 17:12:26 -0400

    run fftw

--- fftw_image_filter.c
+++ fftw_image_filter.c
@@ -137,7 +137,7 @@ int main(int argc, char **argv) {
     }
 
     // feed kernel
-    feed_box_blur_kernel(filter_kernel, FILTER_KERNEL_SIZE);
+    feed_sharpen_kernel(filter_kernel, FILTER_KERNEL_SIZE);
 
     // pad image and filter kernel
     fftw_complex *padded_signal;
@@ -198,7 +198,7 @@ int main(int argc, char **argv) {
             }
         }
     }
-    int result = stbi_write_png("output/fftw/filtered_boxblur_256.png", width, height, 1, output_grey_image, width);
+    int result = stbi_write_png("output/fftw/filtered_sharpen_256.png", width, height, 1, output_grey_image, width);
     if (!result) {
         printf("error writing image!\n");
     }
