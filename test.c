c64d01c360a76fdef9740a857a3c998d404f2ebe
jf773 <jf773@cornell.edu>
2019-05-13 16:07:32 -0400

    fix

--- test.c
+++ test.c
@@ -8,6 +8,11 @@
 #include "self_openmp_fft_lib.h"
 #include <fftw3.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
 int main() {
 
     // load image
