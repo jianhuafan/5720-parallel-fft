2b39863e180847a21c7705e26796e54666edbe31
EC2 Default User <ec2-user@ip-172-31-84-222.ec2.internal>
2019-05-13 20:46:20 +0000

    push result

--- fftw_image_filter.c
+++ fftw_image_filter.c
@@ -118,7 +118,7 @@ void ComplexMul(fftw_complex *a, fftw_complex *b, int size) {
 int main(int argc, char **argv) {
     // load image
     int width, height, bpp;
-    uint8_t* grey_image = stbi_load("input/256.png", &width, &height, &bpp, STBI_grey);
+    uint8_t* grey_image = stbi_load("input/2048.png", &width, &height, &bpp, STBI_grey);
     printf("input image, width: %d, height: %d\n", width, height);
 
     fftw_complex *signal;
@@ -198,7 +198,7 @@ int main(int argc, char **argv) {
             }
         }
     }
-    int result = stbi_write_png("output/fftw/filtered_gaussian_256.png", width, height, 1, output_grey_image, width);
+    int result = stbi_write_png("output/fftw/filtered_gaussian_2048.png", width, height, 1, output_grey_image, width);
     if (!result) {
         printf("error writing image!\n");
     }
